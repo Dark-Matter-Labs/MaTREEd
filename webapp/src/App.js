939640e4f2c11f20fdbdea31d54afe2e51eff477
Gabriele Prestifilippo <gabriele.prestifilippo@welld.ch>
2020-12-04 17:26:27 +0100

    separate pages

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,123 +1,15 @@
-import {useState} from 'react';
-import './App.css';
-import {MapContainer, TileLayer, GeoJSON} from 'react-leaflet'
-import data from './app_data/neighborhoods_madrid_stats.json'
+import {
+  HashRouter,
+  Route,
+} from "react-router-dom";
+import Viewer from './Viewer'
+import Simulator from './Simulator'
 
-const madridCenter = [40.475, -3.70];
-
-
-const Tooltip = ({closeTooltip, style, properties}) =>{
-  // TOTAL TREE NUMBER = 100 (tree_tot)
-  // VALID TREE = 97% (tree_valid)
-  // N. of PERENNIFOLIO = 60 (perc_perennifolio)
-  // N. of CADUCIFOLIO = 40 (perc_caducifoli)
-  // TREE SPECIES
-  // acero (27%) - especie_1 (especie_1_perc)
-  // pinus (15%) - especie_2 (especie_2_perc)
-  // betulla (8%)- especie_3 (especie_3_perc)
-  // others (50%) - “others” (other_perc)
-  // C stock = x ton (sum_c_stock)
-  // C sequestr. = x ton/year (sum_c_seq)
-  
-  if(!properties){
-    return null
-  }
-  const {perc_caducifolio, perc_perennifolio, tree_valid, tree_tot,
-    especie_1_perc, especie_2_perc, especie_3_perc, other_perc,
-    sum_c_stock, sum_c_seq
-  } = properties;
-
-  const treeData=[
-     ['TOTAL TREE NUMBER',tree_tot],
-     ['VALID TREE',tree_valid+'%'],
-     ['Perc. of PERENNIFOLIO',perc_perennifolio],
-     ['Perc. of CADUCIFOLIO',perc_caducifolio],
-  ]
-
-  const treeSpecies=[
-    ['acero ',especie_1_perc],
-    ['pinus',especie_2_perc],
-    ['betulla',especie_3_perc],
-    ['others',other_perc],
- ]
-
- const carbonInfo=[
-  ['C stock ',sum_c_stock],
-  ['C sequestr.',sum_c_seq],
-]
-  return(
-    <div style={style} className='tooltip'>
-        <div className='singleRow'>
-          <p>TREE DATA</p>
-        </div>
-      {treeData.map(([name,val])=>(
-        <div className='row'>
-          <p>{name}</p>
-          <p>{val}</p>
-        </div>
-      ))}
-        <div className='singleRow'>
-          <p>TREE SPECIES</p>
-        </div>
-        {treeSpecies.map(([name,val])=>(
-        <div className='row'>
-          <p>{name}</p>
-          <p>{val}%</p>
-        </div>
-        ))}
-       <div className='singleRow'>
-          <p>CARBON INFO</p>
-        </div>
-          {carbonInfo.map(([name,val])=>(
-          <div className='row'>
-            <p>{name}</p>
-            <p>{val}%</p>
-          </div>
-      ))}
-    <button className='closeButton' onClick={closeTooltip}>Close</button>
-    {/* <button className='simulatorButton' onClick={()=>window.open('Simulator')}>Open Simulator</button> */}
-    </div>
-  )
-}
-
-const App = ()=> {
-  const [tooltipVisible, setTooltipVisible] = useState(false)
-  const [properties, setProperties]= useState(null)
-  const [layerH, setLayerH]= useState(null)
-  const left = tooltipVisible?'0px':'-100%';
+export default function App() {
   return (
-  <div id='container'>
-    
-     <Tooltip 
-     properties={properties}
-     style={{ left: left}}
-     closeTooltip={()=>setTooltipVisible(false)}>
-    </Tooltip>
-
-    <MapContainer id='mapContainer' center={madridCenter} zoom={11} scrollWheelZoom={false}>
-   
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <GeoJSON 
-          onEachFeature={ layer => {}}
-          eventHandlers={{
-            click: async(e) => {
-              console.log('marker clicked', e.layer.feature)
-              setProperties(e.layer.feature.properties)
-              setLayerH(e.layer.feature)
-              setTooltipVisible(true)
-            },
-          }}
-          key={'madrid'} data={data} />
-          {tooltipVisible && layerH && (
-            <GeoJSON 
-            pathOptions={{color:'red'}}
-            key={layerH.properties.NOMBRE} data={layerH} />
-          )}
-    </MapContainer>
-  </div>
-  )
-  }
-export default App;
+    <HashRouter basename='/'>
+     <Route exact path="/" component={Viewer} />
+     <Route path="/simulator" component={Simulator} />
+   </HashRouter>
+  );
+}
--- webapp/src/Simulator.js
+++ webapp/src/Simulator.js
@@ -1,22 +1,49 @@
-import {useState, useRef} from 'react';
 import './App.css';
 import {MapContainer, TileLayer, GeoJSON, MapConsumer} from 'react-leaflet'
 import data from './app_data/neighborhoods_madrid_stats.json'
+import {Link, useLocation} from "react-router-dom";
+import { useEffect, useState, useRef } from 'react';
 
 const madridCenter = [40.475, -3.70];
 
-const App = ()=> {
+const useQuery= ()=> {
+  return new URLSearchParams(useLocation().search);
+}
 
+const Simulator = ()=> {
+  let query = useQuery();
+  const id = query.get("id")
+  const [currentJSON, setCurrentJSON] = useState(data);
+  const currentJSONRef = useRef(null);
+  const mapRef = useRef(null);
 
-  // const [currentJSON, setCurrentJSON]= useState(data)
-  // const currentJSONRef = useRef(null)
-
-
+  useEffect(()=>{
+  if(id){
+              const fileName = `MaTREEd/app_data/trees_neighbours/T_CODBARRIO_${id}.json`;
+              console.log(fileName)
+              fetch(fileName)
+                .then(res=>res.json())
+                .then(data=>{
+                  setCurrentJSON(data)
+                  if(currentJSONRef.current){
+                    const bounds = currentJSONRef.current.getBounds()
+                    console.log(bounds)
+                    mapRef.current.fitBounds(bounds)
+                  }
+                });
+  }
+},[id])
   return (
   <div id='container'>
+    <div className='pageName'>MaTREEd Simulator
+    <Link to="/">
+       <button className='blueButton linkButton'>Go to Viewer</button>
+      </Link>
+    </div>
     <MapContainer id='mapContainer' center={madridCenter} zoom={11} scrollWheelZoom={false}>
     <MapConsumer>
         {(map) => {
+          mapRef.current=map;
       return(
          <>
           <TileLayer
@@ -24,28 +51,29 @@ const App = ()=> {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <GeoJSON 
-          // ref={currentJSONRef}
+          ref={currentJSONRef}
           onEachFeature={ layer => {}}
           eventHandlers={{
             click: async(e) => {
-              console.log('marker clicked', e.layer.feature)
+              console.log('marker clicked', e.layer?.feature?.properties?.CODBARRIO)
 
-              // const CODBARRIO = e.layer.feature.properties.CODBARRIO;
-              // const fileName = `./app_data/trees_neighbours/T_CODBARRIO_${CODBARRIO}.json`;
+              const CODBARRIO = e.layer?.feature?.properties?.CODBARRIO;
+              if(!CODBARRIO){return}
+              const fileName = `MaTREEd/app_data/trees_neighbours/T_CODBARRIO_${CODBARRIO}.json`;
 
-              // fetch(fileName)
-              //   .then(res=>res.json())
-              //   .then(data=>{
-              //     setCurrentJSON(data)
-              //     if(currentJSONRef.current){
-              //       const bounds = currentJSONRef.current.getBounds()
-              //       console.log(bounds)
-              //       map.fitBounds(bounds)
-              //     }
-              //   });
+              fetch(fileName)
+                .then(res=>res.json())
+                .then(data=>{
+                  setCurrentJSON(data)
+                  if(currentJSONRef.current){
+                    const bounds = currentJSONRef.current.getBounds()
+                    console.log(bounds)
+                    map.fitBounds(bounds)
+                  }
+                });
             },
           }}
-          key={currentJSON.name} data={data} />
+          key={currentJSON.name} data={currentJSON} />
           </>
         )}}
       </MapConsumer>
@@ -53,4 +81,4 @@ const App = ()=> {
   </div>
   )
   }
-export default App;
+export default Simulator;
