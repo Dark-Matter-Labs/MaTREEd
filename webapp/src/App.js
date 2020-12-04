c6cbb4b85c6a35d18bed791b3acc2d6f3270dd9b
Gabriele Prestifilippo <gabriele.prestifilippo@welld.ch>
2020-12-04 09:10:03 +0100

    move json

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,35 +1,56 @@
+import {useState, useRef} from 'react';
 import './App.css';
-import {MapContainer, TileLayer, Marker, Popup, GeoJSON} from 'react-leaflet'
-import data from './data/export'
+import {MapContainer, TileLayer, GeoJSON, MapConsumer} from 'react-leaflet'
+import data from './app_data/neighborhoods_madrid_stats.json'
 
-const madridCenter = [40.41, -3.70];
+const madridCenter = [40.475, -3.70];
 
-const App = ()=> (
-<div id='container'>
-  <MapContainer id='mapContainer' center={madridCenter} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <GeoJSON 
-    // onEachFeature={(feature, layer)=>
-    //   console.log(feature)
-    // }
-    eventHandlers={{
-      click: (e) => {
-        console.log('marker clicked', e.layer.feature)
-        alert(e.layer.feature.id)
-      },
-    }}
-    key={'madrid'} data={data} />
+const App = ()=> {
 
-    <Marker position={madridCenter}>
-      <Popup>
-        Popup
-      </Popup>
-    </Marker>
-  </MapContainer>
-</div>
-)
 
+  const [currentJSON, setCurrentJSON]= useState(data)
+  const currentJSONRef = useRef(null)
+
+
+  return (
+  <div id='container'>
+    <MapContainer id='mapContainer' center={madridCenter} zoom={11} scrollWheelZoom={false}>
+    <MapConsumer>
+        {(map) => {
+      return(
+         <>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <GeoJSON 
+          ref={currentJSONRef}
+          onEachFeature={ layer => {}}
+          eventHandlers={{
+            click: async(e) => {
+              console.log('marker clicked', e.layer.feature)
+
+              const CODBARRIO = e.layer.feature.properties.CODBARRIO;
+              const fileName = `./app_data/trees_neighbours/T_CODBARRIO_${CODBARRIO}.json`;
+
+              fetch(fileName)
+                .then(res=>res.json())
+                .then(data=>{
+                  setCurrentJSON(data)
+                  if(currentJSONRef.current){
+                    const bounds = currentJSONRef.current.getBounds()
+                    console.log(bounds)
+                    map.fitBounds(bounds)
+                  }
+                });
+            },
+          }}
+          key={currentJSON.name} data={currentJSON} />
+          </>
+        )}}
+      </MapConsumer>
+    </MapContainer>
+  </div>
+  )
+  }
 export default App;
